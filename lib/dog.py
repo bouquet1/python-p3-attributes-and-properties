48049015fb21b55163e843a57d5697c9f245a8b0
bouquet1 <bukuleta@gmail.com>
2023-08-01 13:12:02 -0700

    3 passed

--- lib/dog.py
+++ lib/dog.py
@@ -12,23 +12,17 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    def __init__(self, name):
+    def __init__(self, name="Fido", breed="Mastiff"):
         self.name = name
+        self.breed = breed
 
     def get_name(self):
         return self.name
     
     def set_name(self, name):
-        # if (type(name)) in str and 1<= len(name) <= 25:
         if isinstance(name, str) and 1 <= len(name) <= 25:
             print(f"Setting name to {name}.")
             self._name = name
         else:
-            print("Name must be a string between 1 and 25 characters.")
+            print("Name must be a string between 1 and 25 characters.\n")
 
-dog = Dog("Fido")
-print(dog.get_name())
-
-
-# The isinstance() function returns True if the specified object is of the specified type, otherwise False.
-#  isinstance(object, type)
